5e5443fae35c90cc2f7a71ea95acdf1940a65476
Kugler Lukas <lukas.kugler@univie.ac.at>
2020-10-16 17:47:52 +0200

    consistent config from cfg.py

--- config/cfg.py
+++ config/cfg.py
@@ -7,17 +7,19 @@ class ExperimentConfiguration(object):
         pass
 
 exp = ExperimentConfiguration()
-exp.expname = "exp_v1.12_LMU_WV73_cde"
+exp.expname = "exp_v1.12_LMU_so_VIS"
 exp.model_dx = 2000
 exp.timestep = 10
-exp.n_ens = 20
-exp.n_nodes = 5
-exp.n_obs = 100  # radar: n_obs for each observation height level
+exp.n_ens = 40
+exp.n_nodes = 10
+exp.n_obs = 81  # radar: n_obs for each observation height level
 
-exp.sat_channels = [6,]
-exp.sat_err = 0.03
-exp.radar_err = 5.
-exp.distance_between_obs_meters = 10000
+vis = dict(sat=True, channel=1, n_obs=n_obs, err_std=0.03, 
+           distance_between_obs_meters=50000,)
+radar = dict(sat=False, kind='radar', n_obs=n_obs, err_std=5.,
+             distance_between_obs_meters=50000,)
+
+exp.observations = [vis,]
 
 # directory paths depend on the name of the experiment
 cluster.expname = exp.expname
