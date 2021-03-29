1fe79b32b1e11c54339b2bf7402ecf0d6a08b99b
lkugler <lukas.kugler@gmail.com>
2021-03-29 20:45:45 +0200

    dart kind misspelled

--- config/cfg.py
+++ config/cfg.py
@@ -9,42 +9,42 @@ class ExperimentConfiguration(object):
 
 
 exp = ExperimentConfiguration()
-exp.expname = "exp_v1.16_P0-4_IR-VIS-WV"
+exp.expname = "exp_v1.16_P0-4_Radar"
 exp.model_dx = 2000
 exp.timestep = 10
 exp.n_ens = 40
 exp.n_nodes = 10
 
-n_obs = 1600  # radar: n_obs for each observation height level
+n_obs = 1600  # so: 64:8x8 rad:10; 10km res: 1600:40x40  # radar: n_obs for each observation height level
 
 vis = dict(plotname='VIS 0.6µm',  plotunits='[1]',
            kind='MSG_4_SEVIRI_BDRF',
            sat_channel=1, n_obs=n_obs, err_std=0.03,
-           cov_loc_radius_km=20)
+           cov_loc_radius_km=32)
 wv73 = dict(plotname='Brightness temperature WV 7.3µm',  plotunits='[K]',
             kind='MSG_4_SEVIRI_TB',
             sat_channel=6, n_obs=n_obs, err_std=False,
-            cov_loc_radius_km=20)
+            cov_loc_radius_km=32)
 ir108 = dict(plotname='Brightness temperature IR 10.8µm', plotunits='[K]',
              kind='MSG_4_SEVIRI_TB',
              sat_channel=9, n_obs=n_obs, err_std=5.,
-             cov_loc_radius_km=20)
+             cov_loc_radius_km=32)
 
 radar = dict(plotname='Radar reflectivity', plotunits='[dBz]',
-             kind='RADAR', 
+             kind='RADAR_REFLECTIVITY', 
              n_obs=n_obs, err_std=5.,
              heights=np.arange(1000, 15001, 1000),
-             cov_loc_radius_km=10, cov_loc_vert_km=2)
+             cov_loc_radius_km=20, cov_loc_vert_km=2)
 
 t2m = dict(plotname='SYNOP Temperature', plotunits='[K]',
-           kind='SYNOP_TEMPERATURE', n_obs=n_obs, err_std=1.0, 
+           kind='SYNOP_TEMPERATURE', n_obs=n_obs, err_std=0.5, 
            cov_loc_radius_km=32, cov_loc_vert_km=1)
 psfc = dict(plotname='SYNOP Pressure', plotunits='[dBz]',
             kind='SYNOP_SURFACE_PRESSURE', n_obs=n_obs, err_std=50.,
             cov_loc_radius_km=32, cov_loc_vert_km=5)
 
 
-exp.observations = [ir108, vis, wv73]  # ir108, wv73, vis]
+exp.observations = [radar, ] #ir108, vis, wv73]  # ir108, wv73, vis]
 
 # directory paths depend on the name of the experiment
 cluster.expname = exp.expname
