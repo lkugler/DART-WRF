563bd41336b40d5f3ae11d90a27bb82dc876f69f
lkugler <lukas.kugler@gmail.com>
2021-06-22 13:16:19 +0200

    fix LES config

--- config/cfg.py
+++ config/cfg.py
@@ -9,28 +9,25 @@ class ExperimentConfiguration(object):
 
 
 exp = ExperimentConfiguration()
-exp.expname = "exp_v1.16_P1-1_Radar"
+exp.expname = "exp_v1.16_P0-4_Radar5+I-test"
 exp.model_dx = 2000
 exp.n_ens = 40
 exp.n_nodes = 10
 
-n_obs = 1600  # 50km res: 64:8x8; 10km res: 1600:40x40  # radar: n_obs for each observation height level
+n_obs = 4 #1600  # 50km res: 64:8x8; 10km res: 1600:40x40  # radar: n_obs for each observation height level
 
 vis = dict(plotname='VIS 0.6µm',  plotunits='[1]',
            kind='MSG_4_SEVIRI_BDRF', sat_channel=1, n_obs=n_obs, 
-           err_std=0.03,
            error_generate=0.03, error_assimilate=0.06,
            cov_loc_radius_km=32)
 
 wv73 = dict(plotname='Brightness temperature WV 7.3µm',  plotunits='[K]',
             kind='MSG_4_SEVIRI_TB', sat_channel=6, n_obs=n_obs, 
-            err_std=False,
-            error_generate=False, error_assimilate=False,
+            error_generate=1., error_assimilate=False,
             cov_loc_radius_km=32)
 
 ir108 = dict(plotname='Brightness temperature IR 10.8µm', plotunits='[K]',
              kind='MSG_4_SEVIRI_TB', sat_channel=9, n_obs=n_obs, 
-             err_std=5.,
              error_generate=5., error_assimilate=10.,
              cov_loc_radius_km=32)
 
@@ -51,7 +48,7 @@ psfc = dict(plotname='SYNOP Pressure', plotunits='[dBz]',
             cov_loc_radius_km=32, cov_loc_vert_km=5)
 
 
-exp.observations = [radar, ] # 108, wv73, vis]
+exp.observations = [vis, wv73, ] # 108, wv73, vis]
 
 # directory paths depend on the name of the experiment
 cluster.expname = exp.expname
