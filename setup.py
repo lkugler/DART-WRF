af2a70550877b6e82e3214ddaf9cd9d72c942163
lkugler <lukas.kugler@gmail.com>
2021-11-24 17:21:00 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,43 @@
+import datetime
+import setuptools
+
+"""
+Install locally:
+>>> pip install climodlab
+"""
+
+YEAR = datetime.date.today().year
+LICENSE_SHORT = f"Copyright (C) {YEAR} ZAMG. All rights reserved"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+def read_requirements(fname):
+    requirements = []
+    with open(fname, "r") as fh:
+        for line in fh:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            requirements.append(line)
+
+
+setuptools.setup(
+    name="DART-WRF",
+    version="1.0.0",
+    author="Lukas Kugler",
+    author_email="lukas.kugler@univie.ac.at",
+    description="Observing system simulation experiments with WRF and DART",
+    long_description=long_description,
+    long_description_content_type="Markdown",
+    url="https://github.com/lkugler/DART-WRF",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.3",
+    install_requires=read_requirements("environment.yml"),
+)
