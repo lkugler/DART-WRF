325e9ba866306e82f1475606e65c12a5004213e2
lkugler <lukas.kugler@gmail.com>
2022-10-18 11:21:45 +0200

    first config

--- dartwrf/run_ens.jet.sh
+++ dartwrf/run_ens.jet.sh
@@ -1,27 +1,37 @@
+module purge
+module load 
 export SLURM_STEP_GRES=none
 
 ##  $SLURM_ARRAY_TASK_ID
 echo "SLURM_ARRAY_TASK_ID:"$SLURM_ARRAY_TASK_ID
+EXPNAME=<exp.expname> 
 
-EXPNAME="OSSE_v1.10_test"
-NAMELIST="namelist.input"
+MAINDIR=<cluster.wrf_rundir_base> 
+pinning=(0-9 10-19 20-29 30-39)
 
-USERDIR=/jetfs/home/lkugler/
-DATADIR=$USERDIR
-SRC_DIR=/jetfs/home/lkugler/compile/WRF/WRF-v4.2/run/
-IDEAL_EXE=/jetfs/home/lkugler/compile/bin/ideal.exe
-WRF_EXE=/jetfs/home/lkugler/compile/bin/wrf-v4.2_v1.10.dmpar.exe
-
-
-# VSC support: be careful with correct pinning !
-pinning=(0-11 12-23 24-35 36-47)
 for ((n=1; n<=4; n++))
 do
     RUNDIR=$USERDIR/run_WRF/$EXPNAME/$IENS
     cd $RUNDIR
     rm -r wrfout_d01_*
-    echo 'mpirun -genv I_MPI_PIN_PROCESSOR_LIST=${pinning[$n]} -np 12 ./wrf.exe >/dev/null 2>&1'
-    mpirun -genv I_MPI_PIN_PROCESSOR_LIST=${pinning[$n]} -np 12 ./wrf.exe >/dev/null 2>&1
+    echo 'mpirun -genv I_MPI_PIN_PROCESSOR_LIST=${pinning[$n]} -np 10 ./wrf.exe >/dev/null 2>&1'
+    mpirun -genv I_MPI_PIN_PROCESSOR_LIST=${pinning[$n]} -np 10 ./wrf.exe >/dev/null 2>&1
     cd ../
 done
 wait
+
+# error checking
+for ((n=1; n<=4; n++))
+do
+   IENS="$(((($SLURM_ARRAY_TASK_ID - 1)* 4) + $n))"
+   RUNDIR=$MAINDIR/$EXPNAME/$IENS
+   cd $RUNDIR
+   line=`tail -n 1 rsl.out.0000`
+   if [[ $line == *"SUCCESS COMPLETE WRF"* ]]; 
+   then 
+      echo $RUNDIR 'SUCCESS COMPLETE WRF'
+   else  
+      echo $RUNDIR $line
+      exit 1
+   fi
+done
