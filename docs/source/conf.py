f236f1dd0349295aad50abe6e03049df8f49c77b
lkugler <lukas.kugler@univie.ac.at>
2023-02-20 17:35:17 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,9 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
