1fdd01221e8aa1e7c0bc08eb68905668ba48a0ce
lkugler <lukas.kugler@gmail.com>
2023-03-14 16:30:07 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ author = 'Lukas Kugler'
 release = '2023.2.21'
 
 # -- General configuration
+sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../'))
 
 master_doc = 'index'
