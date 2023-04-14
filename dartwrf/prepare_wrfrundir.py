da3e0ae3237bf82924ea5ca0acc3bade26d206c7
lkugler <lukas.kugler@gmail.com>
2023-04-14 15:42:37 +0200

    input_profiles optional

--- dartwrf/prepare_wrfrundir.py
+++ dartwrf/prepare_wrfrundir.py
@@ -20,7 +20,6 @@ if __name__ == '__main__':
 
     for iens in range(1, exp.n_ens+1):
         print('preparing ens', iens)
-        input_prof = (exp.input_profile).replace('<iens>', str(iens).zfill(3))
 
         rundir = cluster.wrf_rundir(iens)
         os.makedirs(rundir, exist_ok=True)
@@ -33,5 +32,9 @@ if __name__ == '__main__':
         prepare_namelist.run(iens, begin=init_time, end=dt.datetime(2008, 7, 30, 23),
                             archive=False)
 
-        symlink(input_prof, rundir+'/input_sounding')
+        # prepare input profiles
+        if hasattr(exp, 'input_profile'):
+            input_prof = (exp.input_profile).replace('<iens>', str(iens).zfill(3))
+            symlink(input_prof, rundir+'/input_sounding')
+
     print('finished.')
