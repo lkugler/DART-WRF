9201649774379966fbe1b19ecf9c4d209d6b3609
lkugler <lukas.kugler@gmail.com>
2024-01-18 13:15:52 +0100

    .

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read_requirements(fname):
 
 setuptools.setup(
     name="dartwrf",
-    version="2023.3.31",
+    version="2024.1",
     author="Lukas Kugler",
     author_email="lukas.kugler@univie.ac.at",
     description="Observing system simulation experiments with WRF and DART",
