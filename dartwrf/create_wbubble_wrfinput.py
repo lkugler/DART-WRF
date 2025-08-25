1c7d4aaadee3a35a21cdb467d0d0cd5f889ebb9a
Lukas Kugler <lukas.kugler@univie.ac.at>
2025-08-25 11:45:01 +0200

    update to new configuration scheme

--- dartwrf/create_wbubble_wrfinput.py
+++ dartwrf/create_wbubble_wrfinput.py
@@ -3,29 +3,29 @@ from re import U
 import datetime as dt
 import numpy as np
 
-from dartwrf.exp_config import exp
-from dartwrf.server_config import cluster
+from dartwrf.utils import Config
 import netCDF4 as nc
 
-dx_km = 2
-cr = 15  # km horizontal relaxation distance
-cz = 2000  # meters vertical relaxation distance
 
-if __name__ == "__main__":
-    args = sys.argv[1:]
+def main(cfg):
+    
+    dx_km = 2  # km model resolution
+    cr = 15  # km horizontal relaxation distance
+    cz = 2000  # meters vertical relaxation distance
+    plot = False
 
-    if 'plot' in args:
+    if plot:
         import matplotlib.pyplot as plt
         fig, ax = plt.subplots(figsize=(5,5))
 
-    perturbations = True if 'perturb' in args else False
+    perturbations = True  # if 'perturb' in args else False
     print('update state in wrfinput  wrfout file to DART background file')
     print('perturb wbubble = ', perturbations)
 
 
-    for iens in range(1, exp.ensemble_size+1):
+    for iens in range(1, cfg.ensemble_size+1):
         print('iens', iens)
-        wrfin = cluster.wrf_rundir(iens)+'/wrfinput_d01'
+        wrfin = cfg.dir_wrf_run+'/'+str(iens)+'/wrfinput_d01'
 
         # insert wbubble
         with nc.Dataset(wrfin, 'r+') as ds:
@@ -65,7 +65,7 @@ if __name__ == "__main__":
             incr2[:, :-shift, -shift:] = increment[:, shift:, :shift]  # right part
             incr2[:, -shift:, -shift:] = increment[:, :shift, :shift]  # bottom right corner
 
-            if 'plot' in args:
+            if plot:
                 pdata = incr2[0,:,:]  # select lowest level
                 pdata = pdata - pdata.min()  
                 c = next(ax._get_lines.prop_cycler)['color']
@@ -76,14 +76,18 @@ if __name__ == "__main__":
             ds.variables['THM'][0,...] += incr2
         print(wrfin, 'wbubble inserted.')
 
-    if 'plot' in args:
+    if plot:
         ax.set_aspect(1)
         ax.plot([], [], color='k', lw=1, label='+2 K perturbation')
         ax.plot([], [], color='k', lw=0.5, label='+1 K perturbation')
         ax.legend()
         
-        fout = '/home/fs71386/lkugler/data/analysis/'+exp.expname+'/shiftedbubbles.png'
+        fout = '/home/fs71386/lkugler/data/analysis/'+cfg.name+'/shiftedbubbles.png'
         os.makedirs(os.path.dirname(fout), exist_ok=True)
         fig.savefig(fout, dpi=200) 
         print(fout, 'saved.')
 
+
+if __name__ == '__main__':
+    cfg = Config.from_file(sys.argv[1])
+    main(cfg)
